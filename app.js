f385123ee85bc9027d9999ab4e490a0b81cf930c
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2023-08-07 22:45:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -102,6 +102,6 @@ app.get("/posts/:postName", function(req, res){
 
 
 
-app.listen(30000, function() {
-  console.log("Server started on port 30000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
